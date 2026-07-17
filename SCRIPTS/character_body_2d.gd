f806fa7fb43aa7e1af37aff2f4a5ce3a0d8219ce
ansh33430w <ansh.nothing.in@gmail.com>
2026-07-17 13:58:50 +0530

    added attack dead and hurt to player

--- SCRIPTS/character_body_2d.gd
+++ SCRIPTS/character_body_2d.gd
@@ -2,6 +2,30 @@ extends CharacterBody2D
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hitbox: Area2D = $HITBOX
+@onready var hitboxshape: CollisionShape2D = $HITBOX/hitboxshape
+
+
+var hitboxoffset = 40.0
+var hitbox_vector := {
+	"N": Vector2(0,-1),
+	"NE": Vector2( 1,-1).normalized(),
+	"E" : Vector2(1,0) , 
+	"SE": Vector2(1,1).normalized(),
+	"S": Vector2(0,1) ,
+	"SW": Vector2(-1,1).normalized(),
+	"W": Vector2(-1,0),
+	"NW": Vector2(-1,-1).normalized()
+}
+
+
+
+
+
+
+
+
+
 
 
 const speed = 200
@@ -11,8 +35,18 @@ var facing_direction :Vector2 = Vector2.DOWN
 var direction = ['N','NE','E','SE','S','SW','W','NW']
 
 var isattacking : bool = false
+var ishurt : bool = false
+var isdead : bool = false
+
+var maxhlt = 100
+var hlt = maxhlt
+
+
 
 func _physics_process(delta: float) -> void:
+	print(hlt)
+	if isdead:
+		return
 	var inputdir = Vector2(
 		Input.get_axis("ui_left","ui_right"),
 		Input.get_axis("ui_up","ui_down")
@@ -20,7 +54,7 @@ func _physics_process(delta: float) -> void:
 	).normalized()
 	
 	
-	if Input.is_action_just_pressed("attack") and not isattacking:
+	if Input.is_action_just_pressed("attack") and not isattacking and not ishurt:
 		ATTACK()
 	
 	if isattacking:
@@ -40,13 +74,81 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
+	
+	
+	
+	
+func DAMAGE(amt):
+	if isdead:
+		return
+	hlt-= amt 
+	
+	hlt = max(hlt, 0 )
+	
+	if hlt <= 0:
+		die()
+		
+	else: 
+		hurt()
+	
+	
+func hurt():
+	
+	isattacking = false
+	ishurt = true
+	var dir_name = direction_name(facing_direction)
+	var anim = "HURT_" + dir_name
+	
+	if animated_sprite_2d.sprite_frames.has_animation(anim):
+		animated_sprite_2d.play(anim)
+		
+	else:
+		ishurt = false
+		return
+	
+	if not animated_sprite_2d.animation_finished.is_connected(_on_hurt_finished):
+		animated_sprite_2d.animation_finished.connect(_on_hurt_finished)
+		
+	
+	
+func _on_hurt_finished():
+	if ishurt:
+		ishurt=false
+		animated_sprite_2d.animation_finished.disconnect(_on_hurt_finished)
+	
+
+
+
+
+
+func die():
+	isdead = true
+	isattacking = false
+	ishurt = false
+	
+	velocity = Vector2.ZERO
+	var dir_name = direction_name(facing_direction)
+	var anim = "DIE_" + dir_name
+	if animated_sprite_2d.sprite_frames.has_animation(anim):
+		animated_sprite_2d.play(anim)
+
+
+
+
+
+
+
+
+
 func ATTACK():
+	
 	isattacking= true
 	var dir_name = direction_name(facing_direction)
 	var anim = "ATK_" + dir_name
 	if animated_sprite_2d.sprite_frames.has_animation(anim):
 		animated_sprite_2d.play(anim)
-		
+		hitboxpos(dir_name)
+		hitbox.monitoring = true
 		
 	else:
 		isattacking= false
@@ -60,11 +162,15 @@ func ATTACK():
 	
 	
 	
+func hitboxpos (dirname):
+	var dirvec = hitbox_vector[dirname
+	]
+	hitbox.position = dirvec * hitboxoffset
 	
 func _on_attack_animation_finished():
 	if isattacking:
 		isattacking= false
-		
+		hitbox.monitoring = false
 	
 func animation(name:String):
 	var dir_name = direction_name(facing_direction)
